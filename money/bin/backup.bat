17f71aa2cdce17ac3b94d3b75d89a3fc1e631580
heimdallr <heimdallr@ngs.ru>
2013-02-14 11:48:02 +0700

    Модификация backup

--- money/bin/backup.bat
+++ money/bin/backup.bat
@@ -7,6 +7,7 @@ set gfix=D:\Programs\fb\bin\gfix.exe
 set db=money
 set bak=E:\BackUp\money\MONEY.FDB.fbk
 set log=D:\Sasha\home_compa_dev\money\bin\backup.log
+set arch=D:\Programs\arc\Rar.exe
 
 time /t >> %log% 2>&1
 echo backup >> %log% 2>&1
@@ -14,7 +15,7 @@ echo backup >> %log% 2>&1
 echo sweep >> %log% 2>&1
 %gfix% localhost:money -sweep >> %log% 2>&1
 echo rar >> %log% 2>&1
-rar a -m5 -ag -ep -df %bak%.rar %bak% >> %log% 2>&1
+%arch% a -m5 -ag -ep -df %bak%.rar %bak% >> %log% 2>&1
 time /t >> %log% 2>&1
 echo --------------------------------------------------------------------- >> %log% 2>&1
 echo. >> %log% 2>&1
