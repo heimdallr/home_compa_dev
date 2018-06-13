6f2465de22a2600a1bc5d4281e380258795e1878
heimdallr <heimdallr@ngs.ru>
2018-06-14 00:06:50 +0700

    linear only

--- nn/mnist/main.py
+++ nn/mnist/main.py
@@ -7,8 +7,8 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 
-epochs = 1
-batch_size = 32
+epochs = 10
+batch_size = 200
 rate = 0.01
 momentum = 0.5
 log_interval = 10
@@ -16,33 +16,33 @@ log_interval = 10
 torch.manual_seed(42)
 
 print('===> Loading data')
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('data', train=True, transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))
-                   ])),
-    batch_size=batch_size, shuffle=True)
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('data', train=False, transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))
-                   ])),
-    batch_size=batch_size, shuffle=True)
+
+train_dataset = datasets.MNIST(root='data', 
+                               train=True, 
+                               transform=transforms.ToTensor(),  
+                               download=True)
+
+test_dataset = datasets.MNIST(root='data', 
+                              train=False, 
+                              transform=transforms.ToTensor())
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                           batch_size=batch_size, 
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
+                                          batch_size=batch_size, 
+                                          shuffle=False)
 
 print('===> Building model')
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.fc1 = nn.Linear(784, 800)
+        self.fc2 = nn.Linear(800, 10)
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
+        x = x.view(-1, 784)
         x = F.relu(self.fc1(x))
         x = F.dropout(x)
         x = F.relu(self.fc2(x))
@@ -75,7 +75,7 @@ def test(epoch):
     for data, target in test_loader:
         data, target = Variable(data, volatile=True), Variable(target)
         output = model(data)
-        test_loss += F.nll_loss(output, target).data[0]
+        test_loss += F.nll_loss(output, target).data[0].item()
         pred = output.data.max(1)[1] # get the index of the max log-probability
         correct += pred.eq(target.data).cpu().sum()
 
