bd1ef53c36a4de7b046411d0287abcb1011e7f5d
heimdallr <heimdallr@ngs.ru>
2018-06-14 00:11:31 +0700

    warnings

--- nn/mnist/main.py
+++ nn/mnist/main.py
@@ -46,7 +46,7 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.dropout(x)
         x = F.relu(self.fc2(x))
-        return F.log_softmax(x)
+        return F.log_softmax(x, dim=1)
 
 model = Net()
 
@@ -66,7 +66,7 @@ def train(epoch):
         if (samples_seen // data.size(0)) % log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, samples_seen, len(train_loader.dataset),
-                100. * samples_seen / len(train_loader.dataset), loss.data[0]))
+                100. * samples_seen / len(train_loader.dataset), loss.item()))
 
 def test(epoch):
     model.eval()
@@ -75,7 +75,7 @@ def test(epoch):
     for data, target in test_loader:
         data, target = Variable(data, volatile=True), Variable(target)
         output = model(data)
-        test_loss += F.nll_loss(output, target).data[0].item()
+        test_loss += F.nll_loss(output, target).item()
         pred = output.data.max(1)[1] # get the index of the max log-probability
         correct += pred.eq(target.data).cpu().sum()
 
