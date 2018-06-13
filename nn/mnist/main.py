0e7af4bbe6af6eac004ab09ab5e86150b8201e23
heimdallr <heimdallr@ngs.ru>
2018-06-14 01:03:27 +0700

    spaces to tabs

--- nn/mnist/main.py
+++ nn/mnist/main.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+﻿from __future__ import print_function
 import os
 import torch
 import torch.nn as nn
@@ -17,73 +17,78 @@ torch.manual_seed(42)
 
 print('===> Loading data')
 
-train_dataset = datasets.MNIST(root='data', 
-                               train=True, 
-                               transform=transforms.ToTensor(),  
-                               download=True)
+train_dataset = datasets.MNIST(
+	root='data', 
+	train=True, 
+	transform=transforms.ToTensor(),  
+	download=True)
 
-test_dataset = datasets.MNIST(root='data', 
-                              train=False, 
-                              transform=transforms.ToTensor())
+test_dataset = datasets.MNIST(
+	root='data', 
+	train=False,
+	transform=transforms.ToTensor())
 
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
-                                           batch_size=batch_size, 
-                                           shuffle=True)
+train_loader = torch.utils.data.DataLoader(
+	dataset=train_dataset, 
+	batch_size=batch_size, 
+	shuffle=True)
 
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
-                                          batch_size=batch_size, 
-                                          shuffle=False)
+test_loader = torch.utils.data.DataLoader(
+	dataset=test_dataset, 
+	batch_size=batch_size, 
+	shuffle=False)
 
 print('===> Building model')
 class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(784, 800)
-        self.fc2 = nn.Linear(800, 10)
+	def __init__(self):
+		super(Net, self).__init__()
+		self.fc1 = nn.Linear(784, 800)
+		self.fc2 = nn.Linear(800, 10)
 
-    def forward(self, x):
-        x = x.view(-1, 784)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x)
-        x = F.relu(self.fc2(x))
-        return F.log_softmax(x, dim=1)
+	def forward(self, x):
+		x = x.view(-1, 784)		# Вот это что??? без него не работает. Подозреваю, вместо этого надо входные данные как-то конвертнуть, ы?
+		x = F.relu(self.fc1(x))
+		x = F.relu(self.fc2(x))
+		return F.log_softmax(x, dim=1)
 
 model = Net()
 
 optimizer = optim.SGD(model.parameters(), lr=rate, momentum=momentum)
 
 def train(epoch):
-    model.train()
-    samples_seen = 0
-    for data, target in train_loader:
-        data, target = Variable(data), Variable(target)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.cross_entropy(output, target)
-        loss.backward()
-        optimizer.step()
-        samples_seen += data.size(0)
-        if (samples_seen // data.size(0)) % log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, samples_seen, len(train_loader.dataset),
-                100. * samples_seen / len(train_loader.dataset), loss.item()))
+	model.train()
+	samples_seen = 0
+	for data, target in train_loader:
+		data, target = Variable(data), Variable(target)
+		optimizer.zero_grad()
+		output = model(data)
+		loss = F.cross_entropy(output, target)
+		loss.backward()
+		optimizer.step()
+		samples_seen += data.size(0)
+		if (samples_seen // data.size(0)) % log_interval == 0:
+			print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+				epoch, samples_seen, len(train_loader.dataset),
+				100. * samples_seen / len(train_loader.dataset), loss.item()))
 
 def test(epoch):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    for data, target in test_loader:
-        data, target = Variable(data, volatile=True), Variable(target)
-        output = model(data)
-        test_loss += F.nll_loss(output, target).item()
-        pred = output.data.max(1)[1] # get the index of the max log-probability
-        correct += pred.eq(target.data).cpu().sum()
+	model.eval()
+	test_loss = 0
+	correct = 0
+	for data, target in test_loader:
+		with torch.no_grad():
+			data = Variable(data)
+		target = Variable(target)
+		output = model(data)
+		test_loss += F.nll_loss(output, target).item()
+		pred = output.data.max(1)[1] # get the index of the max log-probability
+		correct += pred.eq(target.data).cpu().sum()
 
-    test_loss = test_loss
-    test_loss /= len(test_loader) # loss function already averages over batch size
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+	test_loss = test_loss
+	test_loss /= len(test_loader) # loss function already averages over batch size
+	print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+		test_loss, correct, len(test_loader.dataset),
+		100. * correct / len(test_loader.dataset)))
 
 def main():
 	for epoch in range(1, epochs + 1):
