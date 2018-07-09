81624e4e32a6f52dfe8e2d1df24d26c10dd9e4d3
heimdallr <heimdallr@ngs.ru>
2018-07-09 09:04:07 +0700

    Регрессируем синусоиду, нормализация входных данных

--- nn/sinreg/main.py
+++ nn/sinreg/main.py
@@ -4,35 +4,19 @@ import torch.nn as nn
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 
-epochs = 10
+epochs = 100000
 batch_size = 10
-rate = 0.001
+rate = 0.1
 momentum = 0.5
 log_interval = 10
 
-x_train = Variable(torch.Tensor([[1.0], [2.0], [3.0]]))
-#y_train = Variable(torch.Tensor([[2.0], [4.0], [6.0]]))
-
-print(x_train)
-print(x_train.size())
-
-xx = x_train * x_train
-xxx = xx * x_train
-xxxx = xxx * x_train
-xxxxx = xxxx * x_train
-xx = torch.cat((x_train, xx, xxx, xxxx, xxxxx), 1)
-
-print(xx)
-print(xx.size())
-
-
-x_train = torch.from_numpy(np.genfromtxt('data/x.txt',delimiter=",",dtype=np.float32)).reshape(-1,1)
+x_train = torch.from_numpy(np.genfromtxt('data/x.txt',delimiter=",",dtype=np.float32)).reshape(-1,1) / 6.28
 x = x_train
 x2 = x * x
 x3 = x2 * x
 x4 = x3 * x
 x5 = x4 * x
-xn = torch.cat((x, x2, x3), 1)
+xn = torch.cat((x, x2, x3, x4), 1)
 
 y_train = torch.from_numpy(np.genfromtxt('data/y.txt',delimiter=",",dtype=np.float32)).reshape(-1,1)
 
@@ -42,7 +26,7 @@ y_test = torch.from_numpy(np.genfromtxt('data/y_.txt',delimiter=",",dtype=np.flo
 class Net(nn.Module):
 	def __init__(self):
 		super(Net, self).__init__()
-		self.fc1 = nn.Linear(3, 1)
+		self.fc1 = nn.Linear(4, 1)
 
 	def forward(self, x):
 		x = self.fc1(x)
